79dd0e9bd5d28995f0074cfd90bd672ca90be2bc
Agnevesh <agneveshsobi@gmail.com>
2026-03-17 02:39:29 +0530

    connect frontend to live backend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:8000/api",
+  baseURL: "https://movie-guesser-api.onrender.com/api",
 })
 
 // automatically add token to every request if logged in
